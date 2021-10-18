85b3778f2515c3d9b7471073743014b00a4314a7
alesyalis <lis.alesia@mail.ru>
2021-10-18 12:17:56 +0300

    fixd

--- Recrutify/Recrutify.Host/Controllers/WeatherForecastController.cs
+++ Recrutify/Recrutify.Host/Controllers/WeatherForecastController.cs
@@ -12,7 +12,7 @@ namespace Recrutify.Host.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching",
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
